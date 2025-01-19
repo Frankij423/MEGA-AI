1c59661cc8ffe7da7345255cea271854eb4f7839
Frankij423 <fjunix123@gmail.com>
2025-01-19 14:25:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '256754358524' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || 'rZ5VmKYD#vRI9vJEujVSzOBif0zjb5Gnl0RiWyQlt4Kz8OWUgpzY' ;  // put your session id here
 
 global.mods = []
 global.prems = []
